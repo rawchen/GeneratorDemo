56f4ccfe6cd555c2ff6164d30486bb3dba6d9ff5
RawChen <2221999792@qq.com>
2021-11-02 22:21:36 +0800

    SpringBoot自动代码生成器

---
+++ src/test/java/com/rawchen/generatordemo/GeneratorDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rawchen.generatordemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratorDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
